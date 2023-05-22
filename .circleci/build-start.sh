d045dcc466bf2feef3fedcf7242951d86e0b8b5f
missgoin <90597569+missgoin@users.noreply.github.com>
2023-05-23 02:14:01 +0700

    Update build-start.sh

--- .circleci/build-start.sh
+++ .circleci/build-start.sh
@@ -4,7 +4,7 @@
 
 # Clone kernel source
 if [[ "$*" =~ "stable" ]]; then
-    git clone --depth=1 https://github.com/missgoin/whyred.git -b caf-ksu kernel
+    git clone --depth=1 https://github.com/missgoin/whyred.git -b main kernel
     cd kernel || exit
 fi
 
